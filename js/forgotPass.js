9126ad03f680cadc8c4ee7462dfd33dac0d43784
Amey Satwe <kadam.yash@northeastern.edu>
2024-11-28 13:14:27 -0500

    testing commits

--- js/forgotPass.js
+++ js/forgotPass.js
@@ -2,7 +2,7 @@ const form = document.getElementById("forgotPassForm");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
+console.log("Changes");
   alert("Please check your email for further instructions.");
 
   form.reset();
